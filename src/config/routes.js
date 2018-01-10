7fbe82950464d5893244bbf14ce55199943e635b
LuizHonorato <lhehonorato@gmail.com>
2018-01-10 13:04:11 -0200

    Consertando arquivo routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const auth = require('./auth')
 
-module.exports = function(server) {
+module.exports = function (server) {
+
     const protectedApi = express.Router()
     server.use('/api', protectedApi)
 
@@ -14,7 +15,7 @@ module.exports = function(server) {
     const openApi = express.Router()
     server.use('/oapi', openApi)
 
-    const AuthService = require('../api/user/AuthService')
+    const AuthService = require('../api/user/authService')
     openApi.post('/login', AuthService.login)
     openApi.post('/signup', AuthService.signup)
     openApi.post('/validateToken', AuthService.validateToken)
